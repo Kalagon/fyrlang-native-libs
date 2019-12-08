a639225c1f1581cca9ede4edfaeba8b8675f1852
Oskar Carl <oskar.carl@uni-due.de>
2019-12-09 00:40:15 +0100

    Rewrite header files

--- include/jemalloc/malloc.h
+++ include/jemalloc/malloc.h
@@ -1,5 +1,7 @@
-void *je_malloc(size_t size);
-void je_free(void *ptr);
-void *je_calloc(size_t nmemb, size_t size);
-void *je_realloc(void *ptr, size_t size);
+#include <stdint.h>
+
+uint8_t* je_malloc(uint64_t size);
+void je_free(uint8_t* ptr);
+uint8_t* je_calloc(uint64_t nmemb, uint64_t size);
+void *je_realloc(void *ptr, uint64_t size);
 
--- include/tcmalloc/malloc.h
+++ include/tcmalloc/malloc.h
@@ -1,5 +1,7 @@
-void *tc_malloc(size_t size);
-void tc_free(void *ptr);
-void *tc_calloc(size_t nmemb, size_t size);
-void *tc_realloc(void *ptr, size_t size);
+#include <stdint.h>
+
+uint8_t* tc_malloc(uint64_t size);
+void tc_free(uint8_t* ptr);
+uint8_t* tc_calloc(uint64_t nmemb, uint64_t size);
+void *tc_realloc(void *ptr, uint64_t size);
 
